98f68c303d087307192c8fb31b37f4c785f0e1a9
willon <willon295@gmail.com>
2019-04-20 16:19:30 +0800

    玄学BUG

--- dist/js/main.js
+++ dist/js/main.js
@@ -45,6 +45,34 @@ function deployDev(v) {
     })
 }
 
+
+function  deployPublish(v) {
+    var v1 = $(v);
+    // 取得值
+    let attr = v1.attr("value");
+    var reg = new RegExp("'", "g");
+    var v2 = attr.replace(reg, "\"");
+    var deployCondition = JSON.parse(v2);
+    var requestHost = "http://localhost:8080";
+    // 异步发送处理
+
+    $.ajax({
+        url: requestHost + "/deploy",
+        type: "POST",
+        data: JSON.stringify(deployCondition),
+        dataType: "JSON",
+        contentType: "application/json",
+        success: function (res) {
+            alert("成功")
+        },
+        error: function (res) {
+            alert("部署失败原因：" + res)
+        }
+
+    })
+
+}
+
 /**
  * 测试通过
  * @param v 参数
